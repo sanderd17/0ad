9efeed5ef7232adf734d584273396c5db2c5554e
Badmadblacksad <badmadblacksad@yahoo.com>
2013-07-10 22:18:58 +0100

    Fix ejabberd module, it was not the client ip that was added to the stanza.

--- source/tools/XpartaMuPP/mod_ipstamp.erl
+++ source/tools/XpartaMuPP/mod_ipstamp.erl
@@ -1,4 +1,3 @@
-
 -module(mod_ipstamp).
 
 -behaviour(gen_mod).
@@ -8,7 +7,7 @@
 -export([start/2, stop/1, on_filter_packet/1]).
 
 %% Domain on which run the ejabberd server
--define (Domain, "localhost").
+-define (Domain, "0ad.mikoton.com").
 
 %% Login of the Xpartamupp jabber client
 -define (XpartamuppLogin, "xpartamupp").
@@ -24,19 +23,20 @@ stop(_Host) ->
 
 on_filter_packet({From, To, Packet} = Input) ->
     {_,STo,_,_,_,_,_} = To,
+    {_,SFrom,_,_,_,_,_} = From,
     if STo == "xpartamupp" ->
       {_,SElement,LPacketInfo,LPacketQuery} = Packet,
       if SElement == "iq" -> 
         {_, SType} = lists:keyfind("type",1,LPacketInfo),
         if SType == "set" ->
-	  {_,_,LXmlns,LGame} = lists:keyfind("query",2,LPacketQuery),
+          {_,_,LXmlns,LGame} = lists:keyfind("query",2,LPacketQuery),
           {_,SXmlns} = lists:keyfind("xmlns",1,LXmlns),
           if SXmlns == "jabber:iq:gamelist" ->
             {_,_,_,LCommand} = lists:keyfind("command",2,LGame),
             {_,SCommand} = lists:keyfind(xmlcdata,1,LCommand),
             if SCommand == <<"register">> ->
               {_,_,KGame,_} = lists:keyfind("game",2,LGame),
-              Info = ejabberd_sm:get_user_info([?XpartamuppLogin],[?Domain],"CC"),
+              Info = ejabberd_sm:get_user_info(SFrom,[?Domain],"0ad"),
               {ip, {Ploc, _Port}} = lists:keyfind(ip, 1, Info),
               SIp = inet_parse:ntoa(Ploc),
               ?INFO_MSG(string:concat("stamp ip: ",SIp), []),
@@ -57,4 +57,5 @@ on_filter_packet({From, To, Packet} = Input) ->
       end
     ; true -> Input 
     end.
+
 
